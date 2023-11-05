6e82095c001d1cbfbd4c08acc615ab5192a87c80
ec50n9 <shelloworld@qq.com>
2023-11-05 10:58:04 +0800

    文件上传处理

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
 import { invoke } from "@tauri-apps/api/tauri";
+import { ArchiveOutline as ArchiveIcon } from "@vicons/ionicons5";
+import { UploadCustomRequestOptions } from "naive-ui";
 
 const handleList = async () => {
   const res = await invoke("get_student_list", {});
@@ -34,6 +36,39 @@ const handleDelete = async () => {
   });
   console.log(res);
 };
+
+const customUploadRequest = ({
+  file,
+  onFinish,
+  onError,
+}: UploadCustomRequestOptions) => {
+  const targetFile = file.file;
+  if (!targetFile) return;
+
+  let func = {
+    // 文本读取
+    "text/plain": () => {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        let contents = event.target?.result as string;
+        console.log(contents);
+      };
+      reader.readAsText(targetFile);
+    },
+    // json导入
+    "application/json": () => {},
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": () => {
+      console.log("excel导入");
+    },
+  }[targetFile.type];
+  if (!func) {
+    console.log("未知类型");
+    onError();
+    return;
+  }
+  func();
+  onFinish();
+};
 </script>
 
 <template>
@@ -44,5 +79,25 @@ const handleDelete = async () => {
     <n-button @click="handleCreate">新增</n-button>
     <n-button @click="handleUpdate">更新</n-button>
     <n-button @click="handleDelete">删除</n-button>
+    <n-upload
+      multiple
+      directory-dnd
+      :max="1"
+      :custom-request="customUploadRequest"
+    >
+      <n-upload-dragger>
+        <div style="margin-bottom: 12px">
+          <n-icon size="48" :depth="3">
+            <archive-icon />
+          </n-icon>
+        </div>
+        <n-text style="font-size: 16px">
+          点击或者拖动文件到该区域来上传
+        </n-text>
+        <n-p depth="3" style="margin: 8px 0 0 0">
+          请不要上传敏感数据，比如你的银行卡号和密码，信用卡号有效期和安全码
+        </n-p>
+      </n-upload-dragger>
+    </n-upload>
   </div>
 </template>
