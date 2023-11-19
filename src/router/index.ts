fb5ae96de3ca8374b3d3f0d65f70e0e957121634
ec50n9 <shelloworld@qq.com>
2023-11-19 14:34:01 +0800

    增加分组管理页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,10 @@ import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
 const routes: RouteRecordRaw[] = [
   { path: "/", component: () => import("@/views/home/index.vue") },
   { path: "/pick-name", component: () => import("@/views/pick-name.vue") },
+  {
+    path: "/group-manager",
+    component: () => import("@/views/group-manager.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/group-manager/index.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <n-el>hello, world</n-el>
+</template>
