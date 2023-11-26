50dd63be50961fbe6a263d7a556d78f4a37a1c5a
ec50n9 <shelloworld@qq.com>
2023-11-26 16:13:38 +0800

    格式化代码

--- src/components/title-bar.vue
+++ src/components/title-bar.vue
@@ -11,13 +11,19 @@ const route = useRoute();
 const router = useRouter();
 
 const canGoBack = computed(() => route.path !== "/");
-const title = computed(()=>route.meta.title||'easy 八宝箱')
+const title = computed(() => route.meta.title || "easy 八宝箱");
 </script>
 
 <template>
   <div data-tauri-drag-region class="px-5 py-2 flex items-center gap-3 b-b">
     <!-- 返回按钮 -->
-    <NButton v-if="canGoBack" class="shrink-0" quaternary circle @click="router.back()">
+    <NButton
+      v-if="canGoBack"
+      class="shrink-0"
+      quaternary
+      circle
+      @click="router.back()"
+    >
       <template #icon>
         <n-icon class="c-gray-7"><arrow-back-round /></n-icon>
       </template>
@@ -31,7 +37,14 @@ const title = computed(()=>route.meta.title||'easy 八宝箱')
     <!-- 操作 -->
     <NSpace class="shrink-0">
       <!-- 最小化 -->
-      <NButton strong secondary circle type="info" size="small" @click="appWindow.minimize()">
+      <NButton
+        strong
+        secondary
+        circle
+        type="info"
+        size="small"
+        @click="appWindow.minimize()"
+      >
         <template #icon>
           <NIcon>
             <MinimizeRound />
@@ -40,7 +53,14 @@ const title = computed(()=>route.meta.title||'easy 八宝箱')
       </NButton>
 
       <!-- 最大化 -->
-      <NButton strong secondary circle type="primary" size="small" @click="appWindow.toggleMaximize()">
+      <NButton
+        strong
+        secondary
+        circle
+        type="primary"
+        size="small"
+        @click="appWindow.toggleMaximize()"
+      >
         <template #icon>
           <NIcon>
             <MaximizeRound />
@@ -49,7 +69,14 @@ const title = computed(()=>route.meta.title||'easy 八宝箱')
       </NButton>
 
       <!-- 关闭 -->
-      <NButton strong secondary circle type="error" size="small" @click="appWindow.close()">
+      <NButton
+        strong
+        secondary
+        circle
+        type="error"
+        size="small"
+        @click="appWindow.close()"
+      >
         <template #icon>
           <NIcon>
             <CloseRound />
